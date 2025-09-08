173db5301cc65db35ac552c8bc27a43ac42622ab
Ashid786 <ashid7821@gmail.com>
2025-09-08 19:46:44 +0530

    Fix CSS2 filename for Welcome page

--- src/pages/Customer_home.jsx
+++ src/pages/Customer_home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import { useNavigate } from "react-router-dom";
 
-import "../styles/customer_home.css";
+import "../styles/Customer_home.css";
 
 import { http } from "../services/api";
 
